0983d3b7f3d20418eb45195fcc7fa83abc2b5929
marwancoder <marwane.ouarradi@gmail.com>
2025-02-03 14:46:27 +0100

    Added loading spinner

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className='flex justify-center'>
+        <img src="spinner.svg" alt="loading" />
+    </div>
+  )
+}
+
+export default Loading
