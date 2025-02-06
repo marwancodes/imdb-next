b26fddc299f0381be2bd71fa4bf4183f893344f5
marwancoder <marwane.ouarradi@gmail.com>
2025-02-06 03:36:45 +0100

    Fix Issue: searchParams is a Promise in Next.js 15

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,16 @@ import Results from '@/components/Results';
 const API_KEY = process.env.API_KEY;
 
 type HomeProps = {
-  searchParams: {
-    genre?: string;
-  };
+  searchParams: Promise<{ genre?: string }>;
 };
 
 const Home = async ({ searchParams }: HomeProps) => {
 
-  const genre = (await searchParams).genre || 'fetchTrending';
+  const { genre } = await searchParams;
+  const selectGenre = genre || 'fetchTrending';
 
   const URL_API = `https://api.themoviedb.org/3${
-      genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
+      selectGenre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
     }?api_key=${API_KEY}&language=en-US&page=1`
 
 
